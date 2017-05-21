1ed92c9c1cef915f6211f90aaf202c1084955aa7
Julio Espinoa-Sokal <julioes@gmail.com>
2017-05-21 10:30:14 -0400

    Create a welcome page route
    Signed-off-by: Julio Espinoa-Sokal <julioes@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
+import {RouterConfiguration, Router} from 'aurelia-router';
+
 export class App {
-  message = 'Hello World!';
+  router: Router;
+  configureRouter(config: RouterConfiguration, router: Router): void {
+    this.router = router;
+    config.title = 'U.S. Census';
+    config.map([
+      { route: '', name: 'welcome', moduleId: 'welcome'}
+    ]);
+  }
 }
